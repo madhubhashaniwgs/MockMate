524a98175ef64c73d65f7e290b32f9eaa2680e24
Saranga <sarangamadhubhashani2001@gmail.com>
2026-08-21 21:39:52 +0530

    chore:dounloard dependencies and edit routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import InterviewSetup from "./pages/InterviewSetup";
+import InterviewResult from "./pages/InterviewResult";
+import InterviewHistory from "./pages/InterviewHistory";
+import MockInterview from "./pages/MockInterview";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/interview-setup" element={<InterviewSetup />} />
+        <Route path="/interview-result" element={<InterviewResult />} />
+        <Route path="/history" element={<InterviewHistory />} />
+        <Route path="/mock-interview" element={<MockInterview />} />
       </Routes>
     </BrowserRouter>
   );
